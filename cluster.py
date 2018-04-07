2f79cbf3a123d7c20db8d2ad287b6fedf6f77def
Fagin Hales <hales.fagin@gmail.com>
2018-04-07 20:50:37 +0100

    misc

--- cluster.py
+++ cluster.py
@@ -27,6 +27,7 @@ def parcheck(inc,outc):
         tempp.if_then(i,Program(NOT(outc)))
     return tempp
 
+
 p = Program([H(1),H(2),H(3),H(4),CZ(0,1),CZ(1,2),CZ(2,3),CZ(3,4)],
                 zmes(0,[0]),ymes(1,[1]),ymes(2,[2]),ymes(3,[3]),
                 parcheck([0,2,3],5),

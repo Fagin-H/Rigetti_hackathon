920c2ad6974400a22f915478c4581f550dc97f44
Fagin Hales <hales.fagin@gmail.com>
2018-04-08 02:09:11 +0100

    qpu

--- cluster_cnot.py
+++ cluster_cnot.py
@@ -10,7 +10,15 @@ from pyquil.quil import Program
 from pyquil import api
 from pyquil.gates import *
 
+from pyquil.api import CompilerConnection, get_devices
+from pyquil.quil import Pragma
+
+devices = get_devices(as_dict=True)
+acorn = devices['19Q-Acorn']
+compiler = CompilerConnection(acorn)
+
 qvm = api.QVMConnection()
+qpu = api.QPUConnection('19Q-Acorn')
 
 def xmes(qu,cl):
     return [H(qu),MEASURE(qu,cl)]
@@ -34,39 +42,76 @@ connectors2 = [(7,2),(1,6)]
 xmeasures = [0,7,2,8,19,14]
 ymeasures = [6,1,12,17,11,18,13]
 
-p = Program([H(i) for i in range(20) if i != 3 and i != 0 and i != 7 and i != 4 and i != 5 and i != 10 and i != 15],
-            zmes(0,25),
-            zmes(7,26),
+p = Program(H(0),H(7),
+            MEASURE(0,25),
+            MEASURE(7,26),
+            [H(i) for i in range(20) if i != 3 and i != 0 and i != 7 and i != 4 and i != 5 and i != 10 and i != 15],
             [CZ(i[0],i[1]) for i in connectors],
 #            SWAP(7,1),
+#            H(7),
+#            zmes(7,26),
 #            [CZ(i[0],i[1]) for i in connectors2],
             
             [xmes(i,i) for i in xmeasures],
             [ymes(i,i) for i in ymeasures],
 
-            parcheck([1,6,17,11],20),
-            parcheck([6,1,18,2,13,14],21),
-            parcheck([0,1,12,17,18,7,8],22),
-            parcheck([7,8,19],23),
+#            parcheck([1,6,17,11],20),
+#            parcheck([6,1,18,2,13,14],21),
+#            parcheck([0,1,12,17,18,7,8],22),
+#            parcheck([7,8,19],23),
+#            
+#            Program().if_then(20,X(16)),
+#            Program().if_then(21,X(9)),
+#            Program().if_then(22,Program(),Z(16)),
+#            Program().if_then(23,Z(9)),
             
-            Program().if_then(20,X(16)),
-            Program().if_then(21,X(9)),
-            Program().if_then(22,Program(),Z(16)),
-            Program().if_then(23,Z(9)),
-            
-            zmes(16,27),
-            zmes(9,28)
+            MEASURE(16,27),
+            MEASURE(9,28)
         )
 
+#job_id = compiler.compile_async(p)
+#job = compiler.wait_for_job(job_id)
+#
+#print('compiled quil', job.compiled_quil())
+#print('gate volume', job.gate_volume())
+#print('gate depth', job.gate_depth())
+#print('topological swaps', job.topological_swaps())
+#print('program fidelity', job.program_fidelity())
+#print('multiqubit gate depth', job.multiqubit_gate_depth())
+data = qpu.run(p,[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,25,26,27,28],1000)
+#
+#for item in data:
+#    print(item[-4:])
 
-#print(qvm.wavefunction(p))
-print(qvm.run(p,[25,26,27,28],5))
-#data = qvm.run(p,[27],100)
-#total = 0
-#for i in data:
-#    total += i[0]
-#print(total)
+realdata = []
+for item in data:
+    if (item[1]+item[6]+item[17]+item[11]
+        ) % 2 == 0 and (item[6]+item[1]+item[18]+item[2]+item[13]+item[14]
+        ) % 2 == 0 and (item[0]+item[1]+item[12]+item[17]+item[18]+item[7]+item[8]
+        ) % 2 == 1 and (item[7]+item[8]+item[19]
+        ) % 2 == 0:
+        realdata.append(item[-4:])
 
+succhance = 0
+
+for item in realdata:
+    if item[0] == 0:
+        if item[2] == 0 and item[1] == item[3]:
+            succhance += 1
+    else:
+        if item[2] == 1 and item[1] != item[3]:
+            succhance += 1
+    
+succhance =  succhance*100/len(realdata)
+    
+print(succhance)
+    
+    
+    
+    
+    
+    
+    
 
 
 

4c1a7f66a1c3a10e606be667293d8e3f6d518f54
Fagin Hales <hales.fagin@gmail.com>
2018-05-09 11:12:30 +0100

    real data

--- cluster_had.py
+++ cluster_had.py
@@ -52,7 +52,7 @@ def generate_had_data(trials):
     for i in range(len(ins)):
         for j in range(len(fins)):
             p = Hadamard(ins[i],fins[j])
-            tempdata = qvm.run(p,[0,1,2,3,4],trials)
+            tempdata = qpu.run(p,[0,1,2,3,4],trials)
             np.save(open('haddata/data'+str(i)+str(j)+'.txt','wb'),tempdata)
     
 def postselect_had_data():
@@ -76,7 +76,7 @@ def generate_pi2_data(trials):
     for i in range(len(ins)):
         for j in range(len(fins)):
             p = pi_over_2(ins[i],fins[j])
-            tempdata = qvm.run(p,[0,1,2,3,4],trials)
+            tempdata = qpu.run(p,[0,1,2,3,4],trials)
             np.save(open('pi2data/data'+str(i)+str(j)+'.txt','wb'),tempdata)
             
 def postselect_pi2(data):
